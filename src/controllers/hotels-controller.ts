f8137a5e3cc891fab45a6e2dc5f6195be86fd7ce
erickssguerra <erickssguerra@gmail.com>
2023-02-04 13:34:58 -0300

    feat: check if user has ticket on route get/hotels

--- src/controllers/hotels-controller.ts
+++ src/controllers/hotels-controller.ts
@@ -1,13 +1,13 @@
 import { AuthenticatedRequest } from '@/middlewares';
-import hotelsService from '@/services/hotels-service';
+import { hotelsService } from '@/services';
 import { Response } from 'express';
 import httpStatus from 'http-status';
 
 export async function getHotels(req: AuthenticatedRequest, res: Response) {
   const { userId } = req;
   try {
-    const listOfHotels = await hotelsService.getHotels(userId);
-    return res.status(httpStatus.OK).send(listOfHotels);
+    const hotels = await hotelsService.getHotels(userId);
+    return res.status(httpStatus.OK).send(hotels);
   } catch (error) {
     if (error.name === 'NotFoundError') {
       return res.status(httpStatus.NOT_FOUND).send(error.message);
--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,14 +1,15 @@
 import { notFoundError } from '@/errors';
 import enrollmentRepository from '@/repositories/enrollment-repository';
 import hotelRepository from '@/repositories/hotel-repository';
+import ticketRepository from '@/repositories/ticket-repository';
 
 async function getHotels(userId: number) {
-  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId)
-  if (!enrollment) throw notFoundError()
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) throw notFoundError();
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket) throw notFoundError();
   const hotels = await hotelRepository.findAllHotels();
-  if (!hotels) {
-    throw notFoundError();
-  }
+  
   return hotels;
 }
 
@@ -16,4 +17,4 @@ const hotelsService = {
   getHotels,
 };
 
-export default hotelsService;
+export { hotelsService };
